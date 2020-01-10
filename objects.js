cabc0d7500405c702107657c3af3a96c706f91b2
Christian Wacker <christianwwacker@gmail.com>
2020-01-10 16:52:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,3 +10,9 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 updatePlayist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+}
+
+removeFromPlaylist(playlist, 'Slowdive');
