81c016c5eb2753f30247d32b923ed8ed1ac41c61
Christian Wacker <christianwwacker@gmail.com>
2020-01-10 16:33:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {
   artist2: 'song2',
   artist3: 'song3'
 };
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, playlist, {[artistName]: songTitle})
+}
